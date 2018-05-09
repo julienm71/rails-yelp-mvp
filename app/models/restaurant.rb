c44257e5d8a49e278184ad41e0ab9cc45c3e6421
Julien Martin <julienm.contact@gmail.com>
2018-05-09 18:00:36 +0200

    fixed validations of edit and new restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,6 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
-  validates :stars, inclusion: { in: [1, 2, 3, 4, 5] }
+  validates :category, presence: true
+  validates :stars, numericality: {only_integer: true}, inclusion: { in: [1, 2, 3, 4, 5] }, presence: true
 end
